78ffba4b2eccaf4247e24dabcf8524e160be9c95
siddhesh1051 <siddhesh1052001@gmail.com>
2023-02-16 01:19:17 +0530

    proxy

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ mongoose
 app.use(
   cors({
     origin: ["https://cineflix-pro.vercel.app"],
-    origin: ["http://localhost:3000"],
+    // origin: ["http://localhost:3000"],
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
   })
