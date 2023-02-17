7205045ed5c75b25abab7c38f2186925d68e0614
siddhesh1051 <siddhesh1052001@gmail.com>
2023-02-17 19:49:02 +0530

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ mongoose
 app.use(
   cors({
     origin: ["https://cineflix-pro.onrender.com"],
-    origin: ["https://cineflix-pro.vercel.app"],
+    // origin: ["https://cineflix-pro.vercel.app"],
     // origin: ["http://localhost:3000"],
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
