fd6fa99f9c1b4a87eb37edb30764098e0f98fc47
siddhesh1051 <siddhesh1052001@gmail.com>
2023-02-17 19:58:02 +0530

    render d

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -44,7 +44,7 @@ module.exports.register = async (req, res, next) => {
       httpOnly: false,
       maxAge: maxAge * 1000,
       withcredentials:true,
-      domain: ".onrender.com",
+      domain: "onrender.com",
       
     });
 
@@ -61,7 +61,7 @@ module.exports.login = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: false, maxAge: maxAge * 1000 ,domain: ".onrender.com" });
+    res.cookie("jwt", token, { httpOnly: false, maxAge: maxAge * 1000 ,domain: "onrender.com" });
     res.status(200).json({ user: user._id, status: true });
     // user.fav.push(movieId);
     // user.fav.push(movieId);
